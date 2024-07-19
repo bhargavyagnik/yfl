1e4b47a356e798c49f276ada31998402c7526a86
bhargavyagnik <bhargavyagnik99@gmail.com>
2024-07-18 23:18:36 -0400

    updates on app

--- src/app.py
+++ src/app.py
@@ -1,43 +1,13 @@
-from flask import Flask, render_template, request, jsonify
-from sqlalchemy import func
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
-db = SQLAlchemy(app)
 
-class Recipe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    ingredients = db.Column(db.String(500), nullable=False)
-    youtube_link = db.Column(db.String(200))
-
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+  search_query = ""
+  if request.method == "POST":
+    search_query = request.form["search"]
+  return render_template("index.html", search_query=search_query)
 
-@app.route('/find_recipes', methods=['POST'])
-def find_recipes():
-    ingredients = request.json['ingredients']
-    matching_recipes = []
-
-    for ingredient in ingredients:
-        recipes = Recipe.query.filter(Recipe.ingredients.like(f'%{ingredient}%')).all()
-        for recipe in recipes:
-            if recipe not in matching_recipes:
-                matching_recipes.append(recipe)
-
-    results = [
-        {
-            'name': recipe.name,
-            'ingredients': recipe.ingredients,
-            'youtube_link': recipe.youtube_link
-        }
-        for recipe in matching_recipes
-    ]
-
-    return jsonify(results)
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":
+  app.run(debug=True)
