b31e2c93008b7fc8cecbaa1edf507b206149a7eb
bhargavyagnik <bhargavyagnik99@gmail.com>
2024-07-25 00:24:06 -0400

    changes in flask app

---
+++ src/flask/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+from elasticsearch import Elasticsearch
+
+app = Flask(__name__)
+es = Elasticsearch(hosts=[{'host': 'elasticsearch', 'port': 9200}])
+
+# Load CSV data into Elasticsearch
+df = pd.read_csv('data/recipes.csv')
+for index, row in df.iterrows():
+    doc = {
+        'recipe_name': row['recipe_name'],
+        'ingredients': row['ingredients'],
+        'preparation_time': row['preparation_time'],
+        'cooking_time': row['cooking_time']
+    }
+    es.index(index='recipes', body=doc)
+
+# Search by name
+@app.route('/search/name', methods=['GET'])
+def search_by_name():
+    query = request.args.get('q')
+    response = es.search(index='recipes', body={
+        'query': {
+            'match': {
+                'recipe_name': query
+            }
+        }
+    })
+    return jsonify(response['hits']['hits'])
+
+# Search by ingredients
+@app.route('/search/ingredients', methods=['GET'])
+def search_by_ingredients():
+    query = request.args.get('q')
+    response = es.search(index='recipes', body={
+        'query': {
+            'match': {
+                'ingredients': query
+            }
+        }
+    })
+    return jsonify(response['hits']['hits'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
